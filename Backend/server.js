8aaf03d4a551aec4227ed3336f333378a6a0ae6f
aryanmishra28 <aryanmishra1851@gmail.com>
2025-09-20 23:37:11 +0530

    updating

--- Backend/server.js
+++ Backend/server.js
@@ -1,9 +1,9 @@
 // start server
 const app=require('./src/app');
-// const connectDB=require('./src/db/db');
-require('dotenv').config(); // Load environment variables from .env file
+const connectDB=require('./src/db/db');
+require('dotenv').config();
 
-// connectDB(); // Connect to the database
+ connectDB(); // Connect to the database
 
 const PORT=process.env.PORT || 5000;
 app.listen(PORT,()=>{
