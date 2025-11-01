bef4088fbab8528ca7ddc5c82c1e9e5155c9cf2d
aryanmishra28 <aryanmishra1851@gmail.com>
2025-11-01 23:47:17 +0530

    change in schema

--- Backend/src/db/db.js
+++ Backend/src/db/db.js
@@ -1,11 +1,17 @@
 const mongoose=require('mongoose');
 
 function connectDB(){
-    mongoose.connect(process.env.MONGO_URI,{
+    const mongoUri = process.env.MONGO_URI;
+    if (!mongoUri) {
+        console.error('MONGO_URI is not defined. Please set it in Backend/.env');
+        process.exit(1);
+    }
+    mongoose.connect(mongoUri,{
     }).then(()=>{
         console.log('Connected to MongoDB');
     }).catch((err)=>{
-        console.log(err);
+        console.error('MongoDB connection error:', err.message);
+        process.exit(1);
     });
 }
 module.exports = connectDB;
