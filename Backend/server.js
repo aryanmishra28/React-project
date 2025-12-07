1d9aefbb5d96e85ed23dc8c6b7aabcb69c23314f
Astha Shukla <asthashukla160428@gmail.com>
2025-12-07 21:40:19 +0530

    preparing to deploy

--- Backend/server.js
+++ Backend/server.js
@@ -3,7 +3,7 @@ const app=require('./src/app');
 const connectDB=require('./src/db/db');
 require('dotenv').config();
 
- connectDB(); // Connect to the database
+connectDB(); // Connect to the database
 
 const PORT=process.env.PORT || 5000;
 app.listen(PORT,()=>{
